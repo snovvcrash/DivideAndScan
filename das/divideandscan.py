fdc63598e2ab12939b86319a6e7c39b1fc95254a
Sam Free5ide <snovvcrash@protonmail.ch>
2021-05-12 08:09:39 +0300

    Add Nmap as init scanner

--- das/divideandscan.py
+++ das/divideandscan.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from collections import namedtuple
 from argparse import ArgumentParser, RawTextHelpFormatter, RawDescriptionHelpFormatter
 
-from das.modules.add import AddMasscanOutput, AddRustscanOutput, AddNaabuOutput
+from das.modules.add import AddMasscanOutput, AddRustscanOutput, AddNaabuOutput, AddNmapOutput
 from das.modules.scan import ScanShow, ScanRun
 from das.modules.report import NmapMerger
 from das.modules.common import BANNER, Logger
@@ -25,11 +25,12 @@ def parse_args():
 	add_epilog = """
 	examples:
 
-	  das add masscan '-e eth0 --rate 1000 -iL hosts.txt --open -p1-65535'
+	  das add masscan '-e eth0 --rate 1000 -iL hosts.txt -p1-65535 --open'
 	  das add rustscan '-b 1000 -t 2000 -u 5000 -a hosts.txt -r 1-65535 -g --no-config --scan-order "Random"'
 	  das add -db testdb -rm naabu '-interface eth0 -rate 1000 -iL hosts.txt -p - -silent -s s'
+	  das add nmap '-n -Pn --min-rate 1000 -T4 -iL hosts.txt -p1-49151 --open -v'
 	""".replace('\t', '')
-	add_parser = subparser.add_parser('add', formatter_class=RawDescriptionHelpFormatter, epilog=add_epilog, help='run a full port scan {masscan,rustscan,naabu} and add the output to DB')
+	add_parser = subparser.add_parser('add', formatter_class=RawDescriptionHelpFormatter, epilog=add_epilog, help='run a full port scan {masscan,rustscan,naabu,nmap} and add the output to DB')
 	add_parser.add_argument('scanner_name', action='store', type=str, help='port scanner name')
 	add_parser.add_argument('scanner_args', action='store', type=str, help='port scanner switches and options')
 	add_parser.add_argument('-db', action='store', type=str, default='das', help='DB name to save the output into')
@@ -106,6 +107,8 @@ def main():
 			AddPortscanOutput = AddRustscanOutput
 		elif args.scanner_name == 'naabu':
 			AddPortscanOutput = AddNaabuOutput
+		elif args.scanner_name == 'nmap':
+			AddPortscanOutput = AddNmapOutput
 		else:
 			logger.print_error(f'{args.scanner_name}: Unsupported port scanner')
 			sys.exit(1)
--- das/modules/add.py
+++ das/modules/add.py
@@ -126,3 +126,30 @@ class AddNaabuOutput(AddOutputBase):
 				hosts.add(ip)
 
 		return (self.portscan_out, len(hosts))
+
+
+class AddNmapOutput(AddOutputBase):
+	"""Child class for processing Nmap output."""
+
+	def parse(self):
+		"""
+		Nmap raw output parser.
+
+		:return: a pair of values (portscan raw output filename, number of hosts added to DB)
+		:rtype: tuple
+		"""
+		hosts = set()
+		for line in self.portscan_raw:
+			try:
+				ip = line.split()[-1]
+				port, proto = line.split()[3].split('/')
+			except:
+				pass
+			else:
+				item = {'ip': ip, 'port': int(port)}
+				if item not in self.db:
+					self.db.insert(item)
+
+				hosts.add(ip)
+
+		return (self.portscan_out, len(hosts))
