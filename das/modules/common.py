e062ec142f698663c22c24c858e10f8635e33d3d
Sam Free5ide <snovvcrash@protonmail.ch>
2021-05-19 00:17:05 +0300

    Increment to v0.1.5

--- das/modules/common.py
+++ das/modules/common.py
@@ -2,7 +2,7 @@
 
 __author__ = '@snovvcrash'
 __site__ = 'https://github.com/snovvcrash/DivideAndScan'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 import time
 from datetime import datetime, timedelta
