21da668f6e4def992216e640d55a7a0b4af4a9e9
Sam Freeside <snovvcrash@protonmail.ch>
2022-10-25 18:27:27 +0300

    Bump v0.3.2

--- das/common.py
+++ das/common.py
@@ -2,7 +2,7 @@
 
 __author__ = '@snovvcrash'
 __site__ = 'https://github.com/snovvcrash/DivideAndScan'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 import time
 from datetime import datetime, timedelta
